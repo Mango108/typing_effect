e608656140b564cdf1d61b0be26de08947f0bead
Mango108 <code.ys108@gmail.com>
2021-12-07 03:15:56 +0900

    타이핑 효과 완성

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
 const lgTxt = document.querySelector(".lgTxt");
-const stringArr = ["I LOVE NEWYORK!", "I LOVE PARIS", "I LOVE AMSTERDAM", "I LOVE MILANO"];
-const stringIndex = stringArr[Math.floor(Math.random() * stringArr.length)];
-const stringSplit = stringIndex.split("");
 
 
+function random(){
+    const stringArr = ["I LOVE NEWYORK!", "I LOVE PARIS", "I LOVE AMSTERDAM", "I LOVE MILANO"];
+    const stringIndex = stringArr[Math.floor(Math.random() * stringArr.length)];
+    const stringSplit = stringIndex.split("");
+
+    return stringSplit;
+}
+
+//리셋 후 타이핑 함수 다시 호출
+function reset(){
+    lgTxt.textContent = ""; //일단 텍스트를 빈값으로
+    blinkTxt(random()); //타이핑 함수 다시 호출
+}
+
+//매개변수가 0보다 클때는 계속 반복적으로 호출, 한 문장 타이핑이 끝나면 리셋 함수 호출
 function blinkTxt(arr){
     if(arr.length>0){
         lgTxt.textContent += arr.shift();
         setTimeout(function(){
-            blinkTxt(stringSplit);
-        },80);
+            blinkTxt(arr);
+        },150); 
+    }else {
+        setTimeout(reset(), 3000);
     }
 }
 
-blinkTxt(stringSplit);
+blinkTxt(random());
 
+
+//커서 깜빡이 효과
 function blink(){
     lgTxt.classList.toggle("active");
 }
